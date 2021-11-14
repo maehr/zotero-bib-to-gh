c3269eeb2491b7e976207d49f1798d43876ae8fa
Moritz Mähr <maehr@users.noreply.github.com>
2021-11-14 16:41:56 +0100

    Update zotero-bib-to-gh.py

--- zotero-bib-to-gh.py
+++ zotero-bib-to-gh.py
@@ -6,56 +6,76 @@ from logzero import logger
 
 def follow_and_extract(url, headers):
     request = httpx.get(url=url, headers=headers)
-    logger.info(f'{url} returned {request.status_code} after {request.elapsed}')
+    logger.info(f"{url} returned {request.status_code} after {request.elapsed}")
     # If there is s link to next url recursive call
     try:
-        next_url = request.links['next'].get('url')
+        next_url = request.links["next"].get("url")
         return request.text + follow_and_extract(next_url, headers)
     # If there is no more link to next url recursion terminates
     except KeyError:
         return request.text
 
 
-zotero_user_id = environ.get('ZOTERO_USER_ID')
+def download_and_write_bib(zotero_headers, zotero_url, file_name="zotero.bib"):
+    zotero_connection = httpx.get(url=zotero_url, headers=zotero_headers)
+    error = f"{zotero_url} returned {zotero_connection.status_code} after {zotero_connection.elapsed}"
+    if zotero_connection.status_code != 200:
+        logger.error(error)
+        exit(error)
+    logger.info(error)
+
+    with open(f"bibliography/{file_name}-last-modified-version", "r") as file:
+        cached_version = int(file.readline())
+        logger.info(f"last-modified-version is {cached_version}")
+
+    latest_version = int(zotero_connection.headers.get("last-modified-version"))
+
+    if cached_version is latest_version:
+        logger.info(
+            f"online version {latest_version} is not different from cache {cached_version}. Done!"
+        )
+        exit()
+
+    logger.info(
+        f"online version {latest_version} is different from cache {cached_version}. Fetching data..."
+    )
+    biblatex_file_content = follow_and_extract(url=zotero_url, headers=zotero_headers)
+
+    with open(f"bibliography/{file_name}", "w") as file:
+        file.write(biblatex_file_content)
+        logger.info("{file_name} updated")
+
+    with open(f"bibliography/{file_name}-last-modified-version", "w") as file:
+        file.write(str(latest_version))
+        logger.info(f"last-modified-version updated to {latest_version}")
+
+
+zotero_user_id = environ.get("ZOTERO_USER_ID")
 if zotero_user_id is None:
-    error = f'ZOTERO_USER_ID not set in GitHub secrets'
+    error = f"ZOTERO_USER_ID not set in GitHub secrets"
+    logger.error(error)
+zotero_group_id = environ.get("ZOTERO_GROUP_ID")
+if zotero_group_id is None:
+    error = f"ZOTERO_GROUP_ID not set in GitHub secrets"
+    logger.error(error)
+if zotero_user_id and zotero_group_id is None:
+    error = f"Neither ZOTERO_USER_ID nor ZOTERO_GROUP_ID set in GitHub secrets"
     logger.error(error)
     exit(error)
-zotero_bearer_token = environ.get('ZOTERO_BEARER_TOKEN')
+zotero_bearer_token = environ.get("ZOTERO_BEARER_TOKEN")
 if zotero_bearer_token is None:
-    error = f'ZOTERO_BEARER_TOKEN not set in GitHub secrets'
+    error = f"ZOTERO_BEARER_TOKEN not set in GitHub secrets"
     logger.error(error)
     exit(error)
 
-zotero_url = f'https://api.zotero.org/users/{zotero_user_id}/items?v=3&format=biblatex'
-zotero_headers = {'Authorization': f'Bearer {zotero_bearer_token}'}
+zotero_user_url = (
+    f"https://api.zotero.org/users/{zotero_user_id}/items?v=3&format=biblatex"
+)
+zotero_group_url = (
+    f"https://api.zotero.org/group/{zotero_group_id}/items?v=3&format=biblatex"
+)
+zotero_headers = {"Authorization": f"Bearer {zotero_bearer_token}"}
 
-with open('bibliography/last-modified-version', 'r') as file:
-    cached_version = int(file.readline())
-    logger.info(f'last-modified-version is {cached_version}')
-
-zotero_connection = httpx.get(url=zotero_url, headers=zotero_headers)
-error = f'{zotero_url} returned {zotero_connection.status_code} after {zotero_connection.elapsed}'
-if zotero_connection.status_code != 200:
-    logger.error(error)
-    exit(error)
-logger.info(error)
-
-latest_version = int(zotero_connection.headers.get('last-modified-version'))
-
-if cached_version is latest_version:
-    logger.info(f'online version {latest_version} is not different from cache {cached_version}. Done!')
-    exit()
-
-logger.info(f'online version {latest_version} is different from cache {cached_version}. Fetching data...')
-biblatex_file_content = follow_and_extract(url=zotero_url, headers=zotero_headers)
-
-with open('bibliography/zotero.bib', 'w') as file:
-    file.write(biblatex_file_content)
-    logger.info('zotero.bib updated')
-
-with open('bibliography/last-modified-version', 'w') as file:
-    file.write(str(latest_version))
-    logger.info(f'last-modified-version updated to {latest_version}')
-
-logger.info('Done!')
+download_and_write_bib(zotero_headers, zotero_user_url)
+download_and_write_bib(zotero_headers, zotero_group_url, f"{zotero_group_id}.bib")
+logger.info("Done!")
